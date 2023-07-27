949a1489ba65666726907700620e1f2bd2c6fdc0
Andrii <lutsiukandrey1@gmail.com>
2023-07-27 18:20:38 +0300

    +

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <PersistGate loading={null} persistor={persistor}>
       <Provider store={store}>
-        <BrowserRouter basename="/news-app">
+        <BrowserRouter basename="/News-App">
           <App />
         </BrowserRouter>
       </Provider>
